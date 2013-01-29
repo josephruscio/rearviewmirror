c3bd744c652964919afd07197d4c2c9e10754e01
Joseph Ruscio <joseph@ruscio>
2013-01-28 22:02:10 -0800

    s/graphite/librato/g

--- README.md
+++ README.md
@@ -1,33 +1,36 @@
-# Ledbetter
+# rearviewmirror
 
-Ledbetter is a simple script for gathering Nagios problem statistics and submitting them to Graphite. It focuses on summary (overall, servicegroup and hostgroup) statistics and writes them to the `nagios.problems` metrics namespace within Graphite.
+rearviewmirror is a simple script for gathering Nagios problem statistics and submitting them to Librato. It focuses on summary (overall, servicegroup and hostgroup) statistics and writes them to the `nagios.problems` metrics namespace within Librato.
+
+rearviewmirror is a fork of [Ledbetter](https://github.com/github/ledbetter).
 
 ## Installation
 
 Clone the GitHub repository and use Bundler to install the gem dependencies.
 
 ```
-$ git clone https://github.com/github/ledbetter.git
-$ cd ledbetter
+$ git clone https://github.com/github/rearviewmirror.git
+$ cd rearviewmirror
 $ bundle install
 ```
 
 ## Usage
 
-Ledbetter requires a number of environment variables for runtime configuration. The following example demonstrates how to run it manually from the command line, but you would typically run it as a cron job.
+rearviewmirror requires a number of environment variables for runtime configuration. The following example demonstrates how to run it manually from the command line, but you would typically run it as a cron job.
 
 ```
 $ export NAGIOS_URL=http://nagios.foo.com/cgi-bin/nagios3
 $ export NAGIOS_USER=foo
 $ export NAGIOS_PASS=bar
-$ export CARBON_URL=carbon://localhost:2003
-$ bundle exec ruby ledbetter.rb
+$ export LIBRATO_USER="foo@bar.com"
+$ export LIBRATO_TOKEN="aec123765489fe45a6"
+$ bundle exec ruby rearviewmirror.rb
 ```
 
-Optionally you can set `VERBOSE=1` to also print statistics to `stdout`. `CARBON_PREFIX` can also be set to override the default namespace (`nagios.problems`).
+Optionally you can set `VERBOSE=1` to also print statistics to `stdout`. `LIBRATO_PREFIX` can also be set to override the default namespace (`nagios.problems`).
 
 ```
-$ VERBOSE=1 bundle exec ruby ledbetter.rb
+$ VERBOSE=1 bundle exec ruby rearviewmirror.rb
 nagios.problems.all 41 1359170720
 nagios.problems.critical 27 1359170720
 nagios.problems.warning 12 1359170720
@@ -41,5 +44,4 @@ nagios.problems.servicegroups.mysql 1 1359170720
 
 ## License 
 
-Ledbetter is distributed under the MIT license.
-
+rearviewmirror is distributed under the MIT license.
---
+++ rearviewmirror.rb
@@ -0,0 +1,78 @@
+
+require 'nagiosharder'
+require 'librato-metrics'
+
+# Ensure we have all the nagios configuration
+nagios_user = ENV['NAGIOS_USER'] || raise('missing NAGIOS_USER')
+nagios_pass = ENV['NAGIOS_PASS'] || raise('missing NAGIOS_PASS')
+nagios_url  = ENV['NAGIOS_URL'] || raise('missing NAGIOS_URL')
+
+# Ensure we have all the librato configuration
+librato_user  = ENV['LIBRATO_USER'] || raise('missing LIBRATO_USER')
+librato_token = ENV['LIBRATO_TOKEN'] || raise('missing LIBRATO_TOKEN')
+
+# Authenticate to Librato
+begin
+  Librato::Metrics.authenticate librato_user, librato_token
+rescue
+  raise "Unable to connect to Librato"
+end
+
+# connect to Nagios server
+begin
+  site = NagiosHarder::Site.new(nagios_url, nagios_user, nagios_pass)
+rescue
+  raise "unable to connect to NAGIOS_URL at #{nagios_url}"
+end
+
+# Create a queue so we only perform a single POST to Librato
+$queue = Librato::Metrics::Queue.new
+
+# XXX: Hack using globals to DRY out the code.
+# If this works and we make a proper gem, we can clean
+# this up.
+#
+# see if we have a custom metrics prefix
+$librato_prefix = ENV['LIBRATO_PREFIX'] || 'nagios.problems'
+
+# grab current timestamp
+$time = Time.now.to_i
+
+def librato_enqueue(k, v)
+  metric = "#{$librato_prefix}.#{k}"
+
+  $queue.add metric => { :measure_time => $time, :value => v}
+
+  if ENV['VERBOSE'].to_i.eql?(1)
+    puts "#{metric} #{v} #{$time}"
+  end
+end
+
+# fetch our service problem counts by type
+critical = site.service_status(:service_status_types => ['critical']).count
+warning = site.service_status(:service_status_types => ['warning']).count
+unknown = site.service_status(:service_status_types => ['unknown']).count
+all = critical + warning + unknown
+
+librato_enqueue 'all', all
+librato_enqueue 'critical', critical
+librato_enqueue 'warning', warning
+librato_enqueue 'unknown', unknown
+
+# fetch our service problem counts by group
+site.servicegroups_summary.each do |name, group|
+  service_problems = group['service_status_counts']['critical'].to_i +
+                     group['service_status_counts']['warning'].to_i +
+                     group['service_status_counts']['unknown'].to_i
+
+  librato_enqueue "servicegroups.#{group['group']}", service_problems
+end
+
+# fetch our host problem counts by group
+site.hostgroups_summary.each do |name, group|
+  down = group['host_status_counts']['down']
+  librato_enqueue "hostgroups.#{group['group']}", down
+end
+
+# Flush the queue to Librato
+queue.submit
